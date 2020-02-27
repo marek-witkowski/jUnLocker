b85586f7acb20c3faf1447cf002efeb8cd12adea
Marek Witkowski <marek.witkowski.priv@gmail.com>
2020-02-27 19:52:43 +0100

    Update Main.java

--- src/eu/intra/unlocker/Main.java
+++ src/eu/intra/unlocker/Main.java
@@ -12,7 +12,7 @@ public class Main
     private static void createAndShowGUI()
     {
         
-        String[] years = {"2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010"};
+        String[] years = {"2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010"};
         
         JFrame frame = new JFrame("Odblokowywacz");
         JPanel panel = new JPanel();
